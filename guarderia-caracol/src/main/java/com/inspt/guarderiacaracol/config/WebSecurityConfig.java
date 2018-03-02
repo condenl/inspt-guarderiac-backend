d0288e770d3ce2d2fe383c4a82babede80c3fabc
Nicolas <nile.conde@gmail.com>
2018-03-02 13:14:08 -0300

    Configuring Spring Security for Basic authentication without oAuth

--- guarderia-caracol/src/main/java/com/inspt/guarderiacaracol/config/WebSecurityConfig.java
+++ guarderia-caracol/src/main/java/com/inspt/guarderiacaracol/config/WebSecurityConfig.java
@@ -4,17 +4,20 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
 	@Autowired
@@ -40,31 +43,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/").permitAll()
-			.antMatchers("/login").permitAll()
-			.antMatchers("/registration").permitAll()
-			.antMatchers("/api/**").permitAll()
-			.antMatchers("/admin/**").hasAuthority("ADMIN")
-			.anyRequest().authenticated()
-			.and().csrf().disable()
-			.formLogin()
-				.loginPage("/login")
+		http.httpBasic()
+			.and()
+				.authorizeRequests()
+				.antMatchers("/index.html", "/", "/api/**").permitAll() //, "/home", "/login"
+				.anyRequest().authenticated()
+			.and()
+				.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and()
+				.formLogin()
+				.loginPage("/")
 				.failureUrl("/login?error=true")
-				.defaultSuccessUrl("/admin/home")
 				.usernameParameter("username")
-				.passwordParameter("password")
-			.and().logout()
-				  .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				  .logoutSuccessUrl("/")
-			.and().exceptionHandling()
-				  .accessDeniedPage("/access-denied");
+				.passwordParameter("password");
 	}
 	
-    @Override
+	@Override
 	public void configure(WebSecurity web) throws Exception {
-    	web.ignoring()
-    		.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    	web.ignoring().antMatchers("/*.bundle.*");
     }
 	
 }
--- guarderia-caracol/src/main/java/com/inspt/guarderiacaracol/web/LoginController.java
+++ guarderia-caracol/src/main/java/com/inspt/guarderiacaracol/web/LoginController.java
@@ -1,70 +1,65 @@
 package com.inspt.guarderiacaracol.web;
 
+import java.security.Principal;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.inspt.guarderiacaracol.domain.AppUser;
 import com.inspt.guarderiacaracol.service.IAppUserService;
 import com.inspt.guarderiacaracol.view.AppUserView;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class LoginController {
 	
 	@Autowired
 	private IAppUserService userService;
 
-	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
-	public ModelAndView login(){
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("login");
-		return modelAndView;
-	}
+//	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+//	public ModelAndView login(){
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.setViewName("login");
+//		return modelAndView;
+//	}
+//	
+//	@RequestMapping(value="/registration", method = RequestMethod.GET)
+//	public ModelAndView registration(){
+//		ModelAndView modelAndView = new ModelAndView();
+//		AppUserView user = new AppUserView();
+//		modelAndView.addObject("user", user);
+//		modelAndView.setViewName("registration");
+//		return modelAndView;
+//	}
 	
-	@RequestMapping(value="/registration", method = RequestMethod.GET)
-	public ModelAndView registration(){
-		ModelAndView modelAndView = new ModelAndView();
-		AppUserView user = new AppUserView();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("registration");
-		return modelAndView;
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public Principal user(Principal user) {
+		return user;
 	}
 	
 	@RequestMapping(value = "/registration", method = RequestMethod.POST)
-	public ModelAndView createNewUser(@Valid AppUserView appUser, BindingResult bindingResult) {
-		ModelAndView modelAndView = new ModelAndView();
+	public @ResponseBody void createNewUser(@Valid AppUserView appUser, BindingResult bindingResult) {
+//		ModelAndView modelAndView = new ModelAndView();
 		AppUser userExists = userService.findByUsername(appUser.getUsername());
 		if (userExists != null) {
 			bindingResult.rejectValue("email", "error.user", "There is already a user registered with the email provided");
 		}
 		if (bindingResult.hasErrors()) {
-			modelAndView.setViewName("registration");
+//			modelAndView.setViewName("registration");
 		} else {
 			userService.save(appUser.toDTO());
-			modelAndView.addObject("successMessage", "User has been registered successfully");
-			modelAndView.addObject("user", new AppUser());
-			modelAndView.setViewName("registration");
+//			modelAndView.addObject("successMessage", "User has been registered successfully");
+//			modelAndView.addObject("user", new AppUser());
+//			modelAndView.setViewName("registration");
 			
 		}
-		return modelAndView;
-	}
-	
-	@RequestMapping(value="/admin/home", method = RequestMethod.GET)
-	public ModelAndView home(){
-		ModelAndView modelAndView = new ModelAndView();
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		AppUser user = userService.findByUsername(auth.getName());
-		modelAndView.addObject("userName", "Welcome " + user.getName() + " (" + user.getUsername() + ")");
-		modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
-		modelAndView.setViewName("admin/home");
-		return modelAndView;
+//		return modelAndView;
 	}
 	
 }
